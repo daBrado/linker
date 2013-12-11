aeec00cdc1100e47461245cb2709cf1924403efb
Braden Pellett <bpellett@ucdavis.edu>
2013-12-11 13:03:49 -0800

    Change Obscured ID algorithm to favor using the existing PRNG

--- lib/obid.rb
+++ lib/obid.rb
@@ -5,27 +5,31 @@
 #
 
 class ObID
-  OFFSET = lambda{|o,r|(o+r+1)*2}
   def initialize(chars, minlen, seed)
-    @chars = chars.sort.uniq.shuffle(random:Random.new(seed))
+    @chars = chars.sort.uniq
     @minlen = minlen
+    @seed = seed
   end
   def str(v)
-    return nil if v<0
-    fn = lambda{|v,offset,result|
+    return nil if v<0 || !v.respond_to?(:times)
+    fn = lambda{|v,rng,result|
       return result if (v == 0 && result.size >= @minlen)
-      q,r = v.divmod @chars.size
-      fn.call(q, OFFSET[offset,r], result+[@chars[(r+offset)%@chars.size]])
+      chars = @chars.shuffle random:rng
+      q,r = v.divmod chars.size
+      (result += [[r,chars[r]]]).map{|r,c|r}.reduce(:+).times.each{rng.rand}
+      fn.call(q, rng, result)
     }
-    fn.call(v,0,[]).join
+    fn.call(v,Random.new(@seed),[]).map{|r,c|c}.join
   end
   def val(s)
-    fn = lambda{|s,offset,result|
-      return result if s.empty?
-      r = (@chars.index(s[0])-offset)%@chars.size
-      fn.call(s[1..-1], OFFSET[offset,r], result+[r])
+    fn = lambda{|s,rng,result|
+      return result if s.size == result.size
+      chars = @chars.shuffle random:rng
+      r = chars.index s[result.size]
+      (result += [r]).reduce(:+).times.each{rng.rand}
+      fn.call(s, rng, result)
     }
-    v = fn.call(s,0,[]) rescue nil
+    v = fn.call(s,Random.new(@seed),[]) rescue nil
     return nil if v == nil || v.size < @minlen || v.drop(@minlen).last == 0
     v.zip(0..Float::INFINITY).map{|v,i|v*@chars.size**i}.reduce(:+)
   end
