9e6995ce582eec4bd203db2abd88770c863113d1
Braden Pellett <bpellett@ucdavis.edu>
2014-01-09 12:26:35 -0800

    Don't assume Net::HTTP thread-safety. Allow exceptions through.

--- lib/redirectapp.rb
+++ lib/redirectapp.rb
@@ -15,7 +15,6 @@ HTTP_SERVICE_UNAVAILABLE = 503
 class RedirectApp
   def initialize(api_uri, log:Logger.new(STDERR))
     @api_uri = URI api_uri
-    @api_http = Net::HTTP.new(@api_uri.hostname, @api_uri.port)
     @log = log
     @log.formatter = lambda{|_,time,_,msg| "#{time.strftime '%FT%T%z'} #{msg}\n"}
   end
@@ -23,7 +22,7 @@ class RedirectApp
     req = Rack::Request.new env
     id = Rack::Utils::unescape(req.path_info.split('/')[1]) rescue nil
     return [HTTP_NOT_FOUND, {}, []] if !id
-    dest_uri = @api_http.request(Net::HTTP::Get.new(File.join(@api_uri.path, 'get', id))).body rescue nil
+    dest_uri = Net::HTTP.new(@api_uri.hostname, @api_uri.port).request(Net::HTTP::Get.new(File.join(@api_uri.path, 'get', id))).body
     return [HTTP_SERVICE_UNAVAILABLE, {}, []] if !dest_uri
     return [HTTP_NOT_FOUND, {}, []] if dest_uri == ''
     @log.info "#{req.ip} #{[req.referer, req.user_agent].map{|s|(s||'-').inspect}.join(' ')} #{id} #{dest_uri.inspect}"
