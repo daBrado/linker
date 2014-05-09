39e9087f6815ab772ee9c97d4f7d81d2c6b8eb19
Braden Pellett <bpellett@ucdavis.edu>
2014-05-08 18:33:31 -0700

    Reuse now existing mutex instead of using thread safe pstore

--- lib/linkstore.rb
+++ lib/linkstore.rb
@@ -15,11 +15,10 @@ class LinkStore
   IDMINLEN = 4
   IDSEED = Random.new_seed
   WORDMINLEN = 3
-  THREAD_SAFE_PSTORE = true
   ULTRA_SAFE_PSTORE = true
   def initialize(file, words: [])
     @wordishes = Wordishes.new(words, WORDMINLEN)
-    @store = PStore.new file, THREAD_SAFE_PSTORE
+    @store = PStore.new file
     @store.ultra_safe = ULTRA_SAFE_PSTORE
     @store.transaction do
       if @store.roots.empty?
@@ -39,16 +38,18 @@ class LinkStore
       embed = x
     end
     uri = URI(uri).to_s
-    @store.transaction do
-      obid = ObID.new(@store[:idchars], @store[:idminlen], @store[:idseed])
-      begin
-        idval = @store[:idnext]
-        @store[:idnext] += 1
-        idstr = obid.str idval
-      end until @wordishes.findin(idstr).empty? && !@store.root?(idstr)
-      uri = uri.gsub(embed, idstr) if embed != nil
-      @store[idstr] = uri
-      idstr
+    @mutex.synchronize do
+      @store.transaction do
+        obid = ObID.new(@store[:idchars], @store[:idminlen], @store[:idseed])
+        begin
+          idval = @store[:idnext]
+          @store[:idnext] += 1
+          idstr = obid.str idval
+        end until @wordishes.findin(idstr).empty? && !@store.root?(idstr)
+        uri = uri.gsub(embed, idstr) if embed != nil
+        @store[idstr] = uri
+        idstr
+      end
     end
   end
   def get(idstr)
